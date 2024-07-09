bf98c18649df3adc73dd92242d01a3297fd40f95
Alex Coulter <ALEXCOULTER.UNC@GMAIL.COM>
2024-07-09 10:24:18 -0400

    updating folder structure

--- soccer-app/src/index.js
+++ soccer-app/src/index.js
@@ -6,9 +6,10 @@ import { AppProvider } from './AppProvider';
 import reportWebVitals from './reportWebVitals';
 
 async function enableMocking() {
-  if (process.env.NODE_ENV !== 'development') {
-    return
-  }
+  console.log("The environment is : " + process.env.NODE_ENV);
+  // if (process.env.NODE_ENV !== 'development') {
+  //   return
+  // }
  
   const { worker } = await import('./mocks/browser');
  
